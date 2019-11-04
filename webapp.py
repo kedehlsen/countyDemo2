065a4b522c6953d8027f4865d29e254e88d1185c
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-11-04 09:40:21 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,3 +1,7 @@
 from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
+app = Flash(__name__)
+
+if __name__=="__main__":
+  app.run(debug=False, port=54321)
