b89fb8151d8c64a1ad572e66b60b5112a18e714c
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-11-05 09:47:31 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Markup, render_template, flash, Markup
+from flask import Flask, request, Markup, render_template, Flash, Markup
 import os
 import json
 app = Flash(__name__)
