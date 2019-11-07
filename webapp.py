2b4b575d2e5770a21632db6facb4dac48fea4534
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-11-07 09:57:10 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,6 +22,7 @@ def get_state_options(counties):
         options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
     return options
 
+@app.route("/fact")
 def get_interesting_fact(state,counties):
     states={}
     for data in counties:
